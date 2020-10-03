bedc5978fc80bc7b227975cdaadcd3cfa1331f3c
X Bit Carry <xbitcarry@gmail.com>
2020-10-03 10:26:52 -0500

    index.php generado con sublimetext

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
+<<!DOCTYPE html>
 <html>
-	<head>
-		<title>X Bit Carry</title>
-	</head>
-	<body>
-
-	</body>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Cambio de precios</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>Programa de cambio de precios</h1>
+</body>
 </html>
